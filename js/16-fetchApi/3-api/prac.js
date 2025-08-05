46f4c511166da17cc046fa235d121a0322afe1f1
larenzzx <larenztabotabo@gmail.com>
2025-08-05 23:11:47 +0800

    added new contents

--- js/16-fetchApi/3-api/prac.js
+++ js/16-fetchApi/3-api/prac.js
@@ -1,35 +1,36 @@
-const pokeImg = document.getElementById("pokeImg");
-const pokeName = document.getElementById("pokeName");
 const form = document.getElementById("searchForm");
 const input = document.getElementById("searchInput");
+const pokeImg = document.getElementById("pokeImg");
+const pokeName = document.getElementById("pokeName");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const pokemonInput = input.value.trim().toLowerCase();
-    if (pokemonInput) {
-        getPokemon(pokemonInput);
-    } else {
-        alert("input pokemon");
-    }
-})
+  e.preventDefault();
+  const pokemonName = input.value.trim().toLowerCase();
+  if (pokemonName) {
+    getPokemon(pokemonName);
+  } else {
+    alert("Please input a Pokemon");
+  }
+});
 
 function getPokemon(name) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        // .then(res => res.json())
-        .then(res => {
-            if (!res.ok) {
-                throw new Error("Pokemon not found");
-            }
-            res.json();
-        })
-        .then(data => {
-            const img = data.sprites.other["official-artwork"].front_default;
-            const name = data.name;
-            pokeImg.src = img;
-            pokeName.textContent = name;
-        })
-        .catch(error => {
-            alert(error.message);
-            console.error(error);
-        })
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Pokemon not found");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const name = data.name;
+      const img = data.sprites.other.dream_world.front_default;
+      console.log(name);
+
+      pokeImg.src = img;
+      pokeName.textContent = name;
+    })
+    .catch((error) => {
+      console.error("Not found", error);
+      alert("Pokemon not found")
+    });
 }
--- js/16-fetchApi/4-api/prac.js
+++ js/16-fetchApi/4-api/prac.js
@@ -1,7 +1,37 @@
 const container = document.querySelector(".container");
 const input = document.getElementById("input");
-
 let allPokemonData = [];
+
+input.addEventListener("input", (e) => {
+  const search = e.target.value.trim().toLowerCase();
+  const filtered = allPokemonData.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search)
+  );
+  renderPokemon(filtered);
+});
+
+function renderPokemon(pokemonList) {
+  container.innerHTML = "";
+  if (pokemonList.length === 0) {
+    container.innerHTML = "<p>No pokemon found</p>";
+    return;
+  }
+
+  pokemonList.forEach((data) => {
+    const img = data.sprites.other.dream_world.front_default;
+    const name = data.name;
+    console.log(name);
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+        <img class="card" src="${img}">
+        <h3>${name}</h3?
+      `;
+
+    container.appendChild(card);
+  });
+}
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
   .then((res) => res.json())
@@ -11,34 +41,7 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
     );
     return Promise.all(pokemonData);
   })
-  .then((pokemonListData) => {
-    allPokemonData = pokemonListData;
+  .then((pokemonDataList) => {
+    allPokemonData = pokemonDataList;
     renderPokemon(allPokemonData);
   });
-
-function renderPokemon(pokemonList) {
-  container.innerHTML = "";
-  if (pokemonList.length === 0) {
-    container.innerHTML = "<p>No pokemon found</p>";
-    return; // much better to have this to stop the render here so it will not continue to the other code
-  }
-  pokemonList.forEach((data) => {
-    const img = data.sprites.other.dream_world.front_default;
-    const name = data.name;
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-      <img class="img" src="${img}">
-        <h3>${name}</h3>
-      `;
-    container.appendChild(card);
-  });
-}
-
-input.addEventListener("input", (e) => {
-  const search = e.target.value.trim().toLowerCase();
-  const filtered = allPokemonData.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(search)
-  );
-  renderPokemon(filtered);
-});
